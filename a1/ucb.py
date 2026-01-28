26443b64a31e248ccf5cea924087dbb0f5a264be
John Cook-Martin <johncookmartin@gmail.com>
2026-01-27 20:12:57 -0500

    Built class to create k armed bandit with dynamic values and probabilities

--- a1/ucb.py
+++ a1/ucb.py
@@ -1,1 +1,28 @@
 # John's Implementation
+
+import random
+import argparse
+from util.bandit_builder import BanditBuilder
+
+
+def main():
+
+    parser = argparse.ArgumentParser(description="UCB Algorithm Implementaion")
+    parser.add_argument("--num_bandits", type=int, default=10)
+    parser.add_argument("--num_rounds", type=int, default=5000)
+    parser.add_argument("--seed", type=int, default=None)
+
+    args = parser.parse_args()
+
+    bandit = BanditBuilder(args.num_bandits, args.seed)
+    for i in range(args.num_bandits):
+        print(f"arm {i}")
+        print(f"value: {bandit.bandit_arr[i]["value"]}")
+        print(f"prob: {bandit.bandit_arr[i]["prob"]}")
+        print(f"expected_value: {bandit.get_expected_value(i)}")
+
+    print(f"optimal_arm: {bandit.get_optimal_action()}")
+
+
+if __name__ == "__main__":
+    main()
---
+++ a1/util/bandit_builder.py
@@ -0,0 +1,36 @@
+import random
+
+
+class BanditBuilder:
+    def __init__(self, num_bandits, seed=None):
+        self.rng = random.Random(seed) if seed else random.Random()
+
+        self.bandit_arr = []
+        self.optimal_action = None
+        self.optimal_action_value = None
+        for i in range(num_bandits):
+            self.bandit_arr.append(self.create_arm())
+            expected_value = self.get_expected_value(i)
+            if (
+                self.optimal_action_value is None
+                or self.optimal_action_value < expected_value
+            ):
+                self.optimal_action = i
+                self.optimal_action_value = expected_value
+
+    def create_arm(self):
+        value = self.rng.randint(1, 10)
+        prob = self.rng.random()
+        return {"value": value, "prob": prob}
+
+    def get_expected_value(self, arm):
+        value = self.bandit_arr[arm]["value"]
+        prob = self.bandit_arr[arm]["prob"]
+        return value * prob
+
+    def get_optimal_action(self):
+        return self.optimal_action
+
+    def pull_arm(self, arm):
+        arm = self.bandit_arr[arm]
+        return self.rng.gauss(arm["mean"], arm["sigma"])
